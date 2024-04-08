21e59f1337dff04f3fa16810539cac16fd75139f
Abdihafith <abdyhafith@gmail.com>
2024-04-08 09:52:27 +0300

    complete

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversedWord = word.split("").reverse().join("");
+
+  if (word.toLowerCase() === reversedWord.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 }
+let word = "madam";
+console.log(isPalindrome(word));
+// isPalindrome(madam)
 
 /* 
   Add your pseudocode here
+  if  letters spell same forwards and backwards,
+  return true
+  else return false(go through each letter to confirm the condition)
+  only strings allowed
+  no empty strings--no spaces
 */
 
 /*
   Add written explanation of your solution here
 */
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
-
   console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
-
 module.exports = isPalindrome;
